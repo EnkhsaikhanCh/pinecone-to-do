732fc5a249e2d0793296f46c91e65d8e91557d80
Enkhsaikhan <ch.enhsaihan@gmail.com>
2023-12-26 20:25:01 +0800

    Connect JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+function showModal() {
+  const modal = document.getElementById("createModal");
+  modal.style.display = "flex";
+  console.log(modal);
+}
+
+function hideModal() {
+  const modal = document.getElementById("createModal");
+  modal.style.display = "none";
+}
