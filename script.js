e4548c00d7421fa8e2424427f22c0fd77ad800d1
Enkhsaikhan <ch.enhsaihan@gmail.com>
2023-12-28 20:00:26 +0800

    JS - Update Modal
    + remove task
    + Each task can have an ID

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-function showModal() {
+// Add title
+let currentTarget = "toDoTasks";
+let generatedId = 0;
+
+function addTask() {
+  const title = document.getElementById("inputTaskTitle").value;
+  const description = document.getElementById("inputTaskDescription").value;
+  const piority = document.getElementById("taskPiority").value;
+
+  if ((title === "", description === "")) {
+    alert("utga oruulnuu!");
+  } else {
+    generatedId++;
+    const taskHtml = `
+      <div id="task-${generatedId}">
+            <div id="toDoTask" class="task">
+              <div class="task-check">
+                <i class="fa-regular fa-circle-check"></i>
+              </div>
+              <div class="task-content">
+                <h4>${title}</h4>
+                <p>${description}</p>
+                <span class="piority">${piority}</span>
+              </div>
+              <div class="task-menu">
+                <i class="fa-regular fa-circle-xmark" onclick="removeTask('task-${generatedId}')"></i
+                ><i class="fa-regular fa-pen-to-square"></i>
+              </div>
+            </div>
+      </div>
+    `;
+
+    const todoElement = document.getElementById(currentTarget);
+
+    todoElement.innerHTML = todoElement.innerHTML + taskHtml;
+  }
+  hideModal();
+  reset();
+}
+
+function showModal(targetId) {
   const modal = document.getElementById("createModal");
   modal.style.display = "flex";
+  currentTarget = targetId;
 }
 
 function hideModal() {
@@ -8,33 +49,11 @@ function hideModal() {
   modal.style.display = "none";
 }
 
+function removeTask(taskId) {
+  document.getElementById(taskId).remove();
+}
+
 function reset() {
   document.getElementById("inputTaskTitle").value = "";
   document.getElementById("inputTaskDescription").value = "";
-}
-// Add title
-function addTask() {
-  const title = document.getElementById("inputTaskTitle").value;
-  const description = document.getElementById("inputTaskDescription").value;
-
-  const toDoElement = document.getElementById("toDoElement");
-  const toDoTask = document.getElementById("toDoTask");
-
-  if ((title === "", description === "")) {
-    alert("utga oruulnuu!");
-  } else {
-    const div = document.createElement("div");
-    const h4 = document.createElement("h4");
-    const p = document.createElement("p");
-    div.innerHTML = toDoTask;
-    h4.innerText = title;
-    p.innerText = description;
-
-    toDoElement.appendChild(div);
-    toDoElement.appendChild(h4);
-    toDoElement.appendChild(p);
-  }
-
-  hideModal();
-  reset();
 }
